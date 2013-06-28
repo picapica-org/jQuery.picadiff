c46e8921b10a902be69c6bc322150f03a652a5cd
Jan Graßegger <jan@anycook.de>
2013-06-28 13:38:13 +0200

    cleaned compareview.js

--- js/views/compareView.js
+++ js/views/compareView.js
@@ -3,9 +3,6 @@
 /**
  * @class  View that holds and controls the comparison presentation of a representation.
 */
-
-//var comparetemplate = "<div class='comparisoncontrol'><div class='controlButtons'><button class='over' >Überlagerung</button><button class='align'>Zeilenangleichung</button><button class='wrap'>Wortumbruch</button></div><div class='slidercontainer'><button class='slider_button_text'>Fundstelle</button><div class='slider'></div><button class='slider_button_source'>Quelle</button></div><div class='sliderstrictcontainer'><div class='strictslider'></div></div></div><div class='refcontainer'><div class='citation'><h5>Fundstelle:</h5><div class='content'></div></div><div class='separator'></div><div class='source'><h5>Quelle:</h5><div class='content'></div></div></div></div>";
-
 var CompareView = Backbone.View.extend(
 
 /** @lends CompareView.prototype */{
@@ -13,10 +10,8 @@ var CompareView = Backbone.View.extend(
 		"click .over" 					: "switchCompareMode",
 		"click .align"					: "switchAlignment",
 		"click .wrap"					: "switchWrap",
-		"mouseover .equal"				: "hoverEqual",
-		"mouseout .equal"				: "unhoverEqual",
-		// "click .slider_button_text" 	: "showSliderText",
-		// "click .slider_button_source"	: "showSliderSource"
+		//"mouseover .equal"			: "hoverEqual",
+		//"mouseout .equal"				: "unhoverEqual",
 	},
 	/**
 	* Called on object construction. Binds events, assigns the data of the diff
@@ -47,15 +42,7 @@ var CompareView = Backbone.View.extend(
 		var template = _.template(templatestring, templateData);
 		this.$el.append(template);
 
-		//this.$("button").button();
-		this.$(".align").addClass("active");
-
-		//this.$(".citation").addClass(this.model.getKategorie().toLowerCase());
-		this.$(".wrap").addClass("active");
-
 		this.render();
-		// this.renderSlider();
-		// this.renderCutSlider();
 
 	},
 	/**
@@ -79,98 +66,7 @@ var CompareView = Backbone.View.extend(
         this.$(".left .content").html(left);
         this.$(".right .content").html(right);
 	},
-	// renderControls : function(){
-	// 	//comparison
- //        if(reference.get("comparison")){
- //        	this.$(".right").removeClass("overlayed")
- //        		.addClass("compareview")
- //        		.css('opacity', 1);
-
-	// 		this.$(".suspicious").css('opacity', 1);
-	// 		this.$(".slidercontainer").hide();
- //        }
- //        //overlay
- //        else{
- //        	this.$(".right").removeClass("compareview")
-	// 			.addClass("overlayed");
-	// 		this.$(".slidercontainer").show();
- //        }
-
-
- //        //alignment
- //        if(this.model.get("alignment")){
-	// 		this.$(".align").addClass("active");
-	// 		this.$(".right br, .citation br").show();
-	// 	}else{
-	// 		this.$(".align").removeClass("active");
-	// 		this.$(".right br, .citation br").hide();
-	// 	}
-
-	// 	//wrap
-	// 	if(this.model.get("wrap")) this.$(".wrap").addClass("active");
-	// 	else this.$(".wrap").removeClass("active");
-
-	// },
-	/**
-	* Draws the slider for the overlay mode.
-	*/
-	// renderSlider : function(){
-	// 	var self = this;
-	// 	this.$(".slider").slider({
-	// 		value : 50,
-	// 		range: "min",
-	// 		animate: true,
-	// 		slide: function(event, ui){
-	// 			opacity = (ui.value+1)/100;
-	// 			if(opacity > 1)
-	// 				opacity=1;
-	// 			self.$(".source").css('opacity', 1-opacity);
-	// 			self.$(".citation").css('opacity', opacity);
-	// 		}
-	// 	});
-	// },
-	/**
-	* Callback function of the source button in the Overlay slider. The source
-	* text is set to full opacity and the reference text is faded out.
-	*/
-	// showSliderSource : function(){
-	// 	this.$(".slider").slider('value', 100);
-	// 	this.$(".citation").css('opacity', 0);
-	// 	this.$(".source").css('opacity', 1);
-	// },
-	/**
-	* Callback function of the reference button in the Overlay slider. The
-	* reference text is set to full opacity and the source text is faded out.
-	*/
-	// showSliderText : function(){
-	// 	this.$(".slider").slider('value', 0);
-	// 	this.$(".citation").css('opacity', 1);
-	// 	this.$(".source").css('opacity', 0);
-	// },
-	/**
-	* Draws and controlls the slider that separates the reference and source
-	* text and also sets the current linelength (in number of characters).
-	*/
-	// renderCutSlider : function(){
-	// 	var self = this;
-
-	// 	var separator = this.$(".separator");
-	// 	var height = this.$(".citation").height();
-	// 	if(height === 0)
-	// 		height = 100;
-	// 	separator.height(height);
-	// 	separator.draggable({
-	// 		axis : "x",
-	// 		drag : function(event, ui){
-	// 			var length = (360 + ui.position.left)*42 / 360;
-	// 			var newLinelength = Math.max(Math.ceil(length), 1);
-	// 			self.$(".source").css("margin-left", ui.position.left);
-	// 			self.model.set("linelength", newLinelength);
-	// 			separator.height(self.$(".citation").height());
-	// 		},
-	// 		containment: this.$(".citation")
-	// 	});
-	// },
+	
 	/**
 	* Callback of the button that controls the compare mode. The comparemode is
 	* toggled.
